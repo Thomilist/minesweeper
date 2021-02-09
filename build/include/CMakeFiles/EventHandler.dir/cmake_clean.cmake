0702bdf6c59c0bd1b6cdcdcfdbaec3c62704abdc
Thomas Emil Jensen <Thomas_98@hotmail.dk>
2021-02-09 20:16:27 +0100

    Project upload

---
+++ build/include/CMakeFiles/EventHandler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EventHandler.dir/eventhandler.cpp.obj"
+  "libEventHandler.a"
+  "libEventHandler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EventHandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/include/CMakeFiles/Minesweeper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Minesweeper.dir/minesweeper.cpp.obj"
+  "libMinesweeper.a"
+  "libMinesweeper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Minesweeper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
